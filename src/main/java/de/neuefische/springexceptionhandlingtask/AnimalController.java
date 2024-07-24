6303273338d339af614eea9052f9f076f948ab09
Marcel Herr <mrclhrr@gmail.com>
2024-07-24 11:07:03 +0200

    add local exceptionhandler

--- src/main/java/de/neuefische/springexceptionhandlingtask/AnimalController.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/AnimalController.java
@@ -1,10 +1,9 @@
 package de.neuefische.springexceptionhandlingtask;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -23,4 +22,16 @@ public class AnimalController {
     String getAllAnimals() {
         throw new NoSuchElementException("No Animals found");
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ErrorMessage(
+                e.getMessage(),
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value()
+        );
+    }
+
+
 }
--- src/main/java/de/neuefische/springexceptionhandlingtask/CarController.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/CarController.java
@@ -1,7 +1,9 @@
 package de.neuefische.springexceptionhandlingtask;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -19,5 +21,16 @@ public class CarController {
     @GetMapping
     String getAllCars() {
         throw new NoSuchElementException("No Cars found");
+    }
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ErrorMessage(
+                e.getMessage(),
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value()
+        );
     }
 }
