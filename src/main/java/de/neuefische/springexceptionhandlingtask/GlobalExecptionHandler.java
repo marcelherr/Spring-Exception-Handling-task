f56618b6e37095046b5e0b9ddee8ff0795d37a46
Marcel Herr <mrclhrr@gmail.com>
2024-07-24 11:22:13 +0200

    add noSuchElementException handler to golbal exception handler

--- src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExecptionHandler.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExecptionHandler.java
@@ -6,17 +6,28 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExecptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorMessage handleIllegalArgumentException(IllegalArgumentException e) {
+    public ErrorMessage handleException(IllegalArgumentException e) {
         return new ErrorMessage(
                 e.getMessage(),
                 LocalDateTime.now(),
                 HttpStatus.BAD_REQUEST.value()
+        );
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorMessage handleException(NoSuchElementException e) {
+        return new ErrorMessage(
+                e.getMessage(),
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value()
         );
     }
 }
